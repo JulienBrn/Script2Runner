6cc8211608444cb7eb53f3f0e97d82e9a34bd11f
JulienB <julienbraine@yahoo.fr>
2025-03-05 13:29:45 +0100

    update

--- src/script2runner/pydantic_cli.py
+++ src/script2runner/pydantic_cli.py
@@ -23,6 +23,12 @@ class ExportSchema(BaseModel):
     schema_file: Path | Literal["stdout"]= Field(default=Path("schema.json"), description="File in which to export the schema")
     export_format : Literal["json", "yaml", "auto"] = Field(default="auto", description="Format in which to export the schema dictionary")
 
+class RunInfo(BaseModel):
+    export_runinfo : bool = Field(default=False, description="Export the current run_info. Stops any other processing.")
+    runinfo_file: Path | Literal["stdout"]= Field(default=Path("run_info.json"), description="File in which to export the run_info")
+    export_format : Literal["json", "yaml", "auto"] = Field(default="auto", description="Format in which to export the run_info")
+
+
 
 class ConfigFile(BaseModel):
     config_file: List[Path] = Field(default=[], description="Config files from which to read arguments values. Config files  will be overwritten by other command line arguments")
@@ -30,6 +36,7 @@ class ConfigFile(BaseModel):
 
 class ExportSchemaCLI(ExportSchema, BaseSettings, cli_parse_args=True, cli_ignore_unknown_args=True, cli_implicit_flags=True): pass
 class ConfigFileCLI(ConfigFile, BaseSettings, cli_parse_args=True, cli_ignore_unknown_args=True, cli_implicit_flags=True): pass
+class RunInfoCLI(RunInfo, BaseSettings, cli_parse_args=True, cli_ignore_unknown_args=True, cli_implicit_flags=True): pass
 
 config_file_dict = {}
 class FileConfigSettingsSource(PydanticBaseSettingsSource):
@@ -48,52 +55,75 @@ class FileConfigSettingsSource(PydanticBaseSettingsSource):
         global config_file_dict
         return config_file_dict
 
-class CLI(ConfigFile,ExportSchema, BaseSettings, cli_parse_args=True, cli_implicit_flags=True):
+class CLI(ExportSchema, RunInfo, ConfigFile, BaseSettings, cli_parse_args=True, cli_implicit_flags=True):
 
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
+        no_run = False
         if "--help" in sys.argv:
             super().__init__(**kwargs)
-        else:
-            export = ExportSchemaCLI()
-            if export.export_schema:
-                schema = self.__class__.model_json_schema()
-                schema["properties"] = {k:v for k,v in schema["properties"].items() if not k in ExportSchema.model_fields and not k in ConfigFile.model_fields}
-                if export.schema_file == "stdout":
-                    if export.export_format=="yaml":
-                        print(yaml.dump(schema))
-                    else:
-                        print(json.dumps(schema))
+            exit()
+        export = ExportSchemaCLI()
+        if export.export_schema:
+            schema = self.__class__.model_json_schema()
+            schema["properties"] = {k:v for k,v in schema["properties"].items() if not k in ExportSchema.model_fields and not k in ConfigFile.model_fields and not k in RunInfo.model_fields}
+            if export.schema_file == "stdout":
+                if export.export_format=="yaml":
+                    print(yaml.dump(schema))
                 else:
-                    if export.export_format=="auto":
-                        export.export_format = export.schema_file.suffix[1:]
-                    with export.schema_file.open("w") as f:
-                        if export.export_format=="yaml":
-                            yaml.safe_dump(schema, f)
-                        else:
-                            json.dump(schema, f, indent=4)
-                exit()
-
-            config_files: ConfigFile = ConfigFileCLI()
-            c = {}
-            for p in config_files.config_file:
-                if not p.exists():
-                    raise Exception(f"File {p} does not exist")
-                with p.open("r") as f:
-                    if p.suffix==".json":
-                        d = json.load(f) 
-                    elif p.suffix in [".yml", ".yaml"]:
-                        d = yaml.safe_load(f)
+                    print(json.dumps(schema))
+            else:
+                if export.export_format=="auto":
+                    export.export_format = export.schema_file.suffix[1:]
+                with export.schema_file.open("w") as f:
+                    if export.export_format=="yaml":
+                        yaml.safe_dump(schema, f)
                     else:
-                        raise Exception(f"Unknown suffix {p.suffix} for file {p}")
-                c.update(d)
-            global config_file_dict
-            config_file_dict = c
-            try:
-                super().__init__(**kwargs)
-            except Exception as e:
-                error_info = "\n\t".join(str(e).split("\n")[1:-1])
-                logger.error("Error while reading arguments\n\t" + error_info + "\n\tUse the --help option to display command line usage")
-                exit() 
+                        json.dump(schema, f, indent=4)
+            no_run = True
+
+        if not hasattr(self, "_run_info"):
+            self._run_info = {}
+
+        run_info_export = RunInfoCLI()
+        if run_info_export.export_runinfo:
+            if run_info_export.runinfo_file == "stdout":
+                if run_info_export.export_format=="yaml":
+                    print(yaml.dump(self._run_info))
+                else:
+                    print(json.dumps(self._run_info))
+            else:
+                if run_info_export.export_format=="auto":
+                    run_info_export.export_format = run_info_export.schema_file.suffix[1:]
+                with run_info_export.runinfo_file.open("w") as f:
+                    if run_info_export.export_format=="yaml":
+                        yaml.safe_dump(self._run_info, f)
+                    else:
+                        json.dump(self._run_info, f, indent=4)
+            no_run = True
+
+        if no_run:
+            exit()
+        config_files: ConfigFile = ConfigFileCLI()
+        c = {}
+        for p in config_files.config_file:
+            if not p.exists():
+                raise Exception(f"File {p} does not exist")
+            with p.open("r") as f:
+                if p.suffix==".json":
+                    d = json.load(f) 
+                elif p.suffix in [".yml", ".yaml"]:
+                    d = yaml.safe_load(f)
+                else:
+                    raise Exception(f"Unknown suffix {p.suffix} for file {p}")
+            c.update(d)
+        global config_file_dict
+        config_file_dict = c
+        try:
+            super().__init__(**kwargs)
+        except Exception as e:
+            error_info = "\n\t".join(str(e).split("\n")[1:-1])
+            logger.error("Error while reading arguments\n\t" + error_info + "\n\tUse the --help option to display command line usage")
+            exit() 
 
             
     @classmethod
